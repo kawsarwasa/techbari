cf784fb0cd6d18386315b774503cbf704634ca44
kawsarwasa <kawsarwasa@gmail.com>
2026-09-09 10:30:56 +0600

    Enable Django clickjacking protection

--- techbari/settings.py
+++ techbari/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "staff_access.middleware.StaffAccessMiddleware",
 ]
 
